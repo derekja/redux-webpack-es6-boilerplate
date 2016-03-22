2d9e6f55d79c8c1ce096507bc07bb12556017a4a
lyramer <andy.wynden@gmail.com>
2016-03-21 23:51:46 -0700

    removed expletives from MarkdownApp and fixed mathy bits.

--- src/js/containers/MrkdownApp/MrkdownApp.js
+++ src/js/containers/MrkdownApp/MrkdownApp.js
@@ -6,15 +6,16 @@ import MarkdownIt from 'markdown-it';
 import mk from 'markdown-it-katex';
 
 
-var txt = '**fuck markdown**: sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / 4)^2';
-var txt2 = 'c = \\pm\\sqrt{a^2 + b^2}';
-var txt3 = '# Math Rulez! \n  $\\sqrt{3x-1}+(1+x)^2$';
+var txt = '**bugger markdown**: $sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / 4)^2$';
+var txt2 = 'c = $\\pm\\sqrt{a^2 + b^2}$ \n';
+var txt3 = '# Math Rulez! \n  $\\sqrt{3x-1}+(1+x)^2$ \n';
+var txt4 = txt3.concat(txt2);
 
 var md = new MarkdownIt();
 
 
 md.use(mk);
-var foo = md.render(txt3);
+var foo = md.render(txt);
  
 
 
@@ -25,7 +26,7 @@ class Mrkdown extends Component {
     return (
       <div>
         <h2>Markdown</h2>
-        <div className="mrkdown" dangerouslySetInnerHTML={{__html: foo}}>
+        <div className="Mrkdown" dangerouslySetInnerHTML={{__html: foo}}>
         </div>
       </div>
     )
