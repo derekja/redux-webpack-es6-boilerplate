33a25004a99d40a0ead91e65123ee1f63249e5d9
derekja <derekja@gmail.com>
2016-03-23 13:36:15 -0700

    just updated the math markdown to show symbols better

--- src/js/containers/MrkdownApp/MrkdownApp.js
+++ src/js/containers/MrkdownApp/MrkdownApp.js
@@ -6,7 +6,7 @@ import MarkdownIt from 'markdown-it';
 import mk from 'markdown-it-katex';
 
 
-var txt = '**bugger markdown**: $sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / 4)^2$';
+var txt = '**bugger markdown**: \n $\\sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - \\sin(\\pi / 4)^2$';
 var txt2 = 'c = $\\pm\\sqrt{a^2 + b^2}$ \n';
 var txt3 = '# Math Rulez! \n  $\\sqrt{3x-1}+(1+x)^2$ \n';
 var txt4 = txt3.concat(txt2);
