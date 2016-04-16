ece8cb1fa7ddf69676cfd84a329b2a00d720c9bc
derekja <derekja@gmail.com>
2016-04-15 17:18:16 -0700

    react-grid-layout basic example

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
+'use strict';
 import '../styles/bootstrap.min.css';
 import '../styles/styles.scss';
+import '../../node_modules/react-grid-layout/node_modules/react-resizable/css/styles.css';
+import '../../node_modules/react-grid-layout/css/styles.css';
 
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
@@ -8,8 +11,27 @@ import configureStore  from './store/configureStore';
 import { Router, browserHistory, hashHistory } from 'react-router';
 import { connect } from 'react-redux';
 
-
+var ReactGridLayout = require('react-grid-layout');
 import routes from './routes';
+
+
+var MyFirstGrid = React.createClass({
+  render: function() {
+    // layout is an array of objects, see the demo for more complete usage
+    var layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+    return (
+      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key={'a'}>a</div>
+        <div key={'b'}>b</div>
+        <div key={'c'}>c</div>
+      </ReactGridLayout>
+    )
+  }
+});
 
 const store = configureStore();
 const rootElement = document.getElementById('app');
@@ -39,7 +61,7 @@ if (process.env.NODE_ENV !== 'production') {
 // Render the React application to the DOM
 ReactDOM.render(
   <Provider store={store}>
-    {ComponentEl}
+    <MyFirstGrid/>
   </Provider>,
   rootElement
 );
