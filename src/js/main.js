946ea4fab3f0fc12fcc4961bf2a77a646f210d37
derekja <derekja@gmail.com>
2016-04-16 12:52:10 -0700

    sample golden layout component working

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux';
 import configureStore  from './store/configureStore';
 import { Router, browserHistory, hashHistory } from 'react-router';
 import { connect } from 'react-redux';
-
+import About from './containers/AboutApp/AboutApp';
+import Mrkdown from './containers/MrkdownApp/MrkdownApp';
+import LMap from './containers/Map/Map.js';
 
 import routes from './routes';
 
@@ -36,37 +38,49 @@ if (process.env.NODE_ENV !== 'production') {
   );
 }
 
-var TestComponent = React.createClass({
-    render: function() {
-        return (<h1>{this.props.label}</h1>)
+class testComponent extends Component {
+    render() {
+        
+        return (<h3><i>{this.state.label}foo</i></h3>)
     }
-});
+}
 
 
+var AboutComp = function(container) {
+  var abt = container.getElement()[ 0 ];
+  ReactDOM.render(<About/>, abt);
+  
+};
 
 var myLayout = new GoldenLayout({
     content: [{
         type: 'row',
         content:[{
             type:'component',
-            componentName: 'testComponent',
-            componentState: { label: 'A' }
+            componentName: 'About',
+            title: 'item A',
+            ComponentState: { label: 'A' }
         },{
             type: 'column',
             content:[{
                 type:'component',
-                componentName: 'testComponent',
-                componentState: { label: 'B' }
+                componentName: 'Mrkdown',
+                title: 'item b',
+                props: { label: 'B' }
             },{
                 type:'component',
-                componentName: 'testComponent',
-                componentState: { label: 'C' }
+                componentName: 'LMap',
+                title: 'item C',
+                props: { label: 'C' }
             }]
         }]
     }]
 });
 
-myLayout.registerComponent( 'testComponent', TestComponent );
+myLayout.registerComponent( 'TestComponent', testComponent );
+myLayout.registerComponent( 'About', AboutComp );
+myLayout.registerComponent( 'Mrkdown', Mrkdown );
+myLayout.registerComponent( 'LMap', LMap );
 
 //Once all components are registered, call
 myLayout.init();
