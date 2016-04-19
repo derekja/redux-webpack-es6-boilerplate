a40acf49503118b449ffd06483c4a3f4bc7299eb
Bing Gao <gaobing.cp@gmail.com>
2016-04-19 15:16:58 -0700

    put multi layer code does not work yet

--- src/js/containers/Map/Map.js
+++ src/js/containers/Map/Map.js
@@ -9,21 +9,36 @@ import './Map.scss';
 var LeafletMap = React.createClass({
     
     componentDidMount: function() {
+        
+   // var  mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        
+    var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>',thunLink = '<a href="http://thunderforest.com/">Thunderforest</a>';
+
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',osmAttrib = '&copy; ' + osmLink + ' Contributors',landUrl = 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',thunAttrib = '&copy; '+osmLink+' Contributors & '+thunLink;
+
+var osmMap = L.tileLayer(osmUrl, {attribution: osmAttrib}), landMap = L.tileLayer(landUrl, {attribution: thunAttrib});
+        
+        
         var map = this.map = L.map(ReactDOM.findDOMNode(this), {
             minZoom: 2,
             maxZoom: 20,
             layers: [
-         L.tileLayer(
-         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+       L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         {attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'})
             ],
             attributionControl: false,
         });
-      map.setView([45.5200,-122.6716007], 13);   
+       //  var marker = L.marker([-41.29042, 174.78219]).addTo(map);
+      //map.setView([45.5200,-122.6716007], 13); 
+      var baseLayers = {
+	"OSM Mapnik": osmMap,
+	"Landscape": landMap
+};  
+//L.control.layers(baseLayers).addTo(map);
      // L.control.zoom({ position: "bottomright"}).addTo(map);
-      map.locate({setView: true, maxZoom: 16});
+     // map.locate({setView: true, maxZoom: 16});
       map.zoomControl.setPosition('topright');
-   // L.control.scale({ position: "topright"}).addTo(map);
+    L.control.scale({ position: "topright"}).addTo(map);
    // L.control.fullscreen().addTo(map);
      //   map.on('click', this.onMapClick);
         
