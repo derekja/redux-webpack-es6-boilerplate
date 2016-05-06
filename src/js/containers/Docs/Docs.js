158d7d7c7dfc1619c387b0d1de2fd7f75a7cdde4
Andy <andy.wynden@gmail.com>
2016-05-06 00:45:03 -0700

    Added wiki txt files and beginning implementation of scroll bar

--- src/js/containers/Docs/Docs.js
+++ src/js/containers/Docs/Docs.js
@@ -2,21 +2,97 @@ import './Docs.scss';
 import React, { Component, PropTypes } from 'react';
 import MarkdownIt from 'markdown-it';
 import mk from 'markdown-it-katex';
+import Scroll from 'react-scroll'; 
 
-
-var wikitxt = '##### URTHECAST DEVELOPMENT PLAYGROUND\n\nThe algorithms test kitchen is designed to allow quick experimentation with urthecast data in a flexible programming environment that requires virtually no setup. The user interface is at a very early stage and will change significantly over the coming weeks, but it is already functional. The playground can be accessed at [http://urthecast-rd.dev.urthecast.com/viewer/](http://urthecast-rd.dev.urthecast.com/viewer/) \n \n If you do not have access then please submit a request with your IP address. Soon it will be driven by gitlab permissions instead of needing a whitelist of IPs. \n \n The scripts are written in python, version 2.7.5, and the programming model is based on a MapReduce paradigm. The tile script is executed on each individual tile in the request. (A request is currently the viewport in the web browser, although this will be expanded in the future.) Each tile script can send data to the collector. The collector is executed across the set of tiles when all results in the request have been returned. \n \n Currently, when the scripts are updated (both tile and collector) the user will push the "update algorithms" button and the tile script will begin to execute on each tile in the request. When the user presses the "view results" button the collector script will be executed.';
-var txt = '**bugger markdown**: \n $\\sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - \\sin(\\pi / 4)^2$';
-var txt2 = 'c = $\\pm\\sqrt{a^2 + b^2}$ \n';
-var txt3 = '# Math Rulez! \n  $\\sqrt{3x-1}+(1+x)^2$ \n';
-var txt4 = txt3.concat(txt2);
+var Home_txt = require("raw!./Wiki_Home.txt");
+var Metadata_txt = require("raw!./Wiki_Metadata.txt");
+var TileScript_txt = require("raw!./Wiki_TileScript.txt");
 
 var md = new MarkdownIt();
 
-
 md.use(mk);
-var foo = md.render(wikitxt);
- 
+var foo = md.render(Home_txt);
+//var foo1 = md.render(Metadata_txt);
+//var foo2 = md.render(TileScript_txt);
 
+var Link       = Scroll.Link;
+var DirectLink = Scroll.DirectLink;
+var Element    = Scroll.Element;
+var Events     = Scroll.Events;
+var scroll     = Scroll.animateScroll;
+
+          <div className="Docs" dangerouslySetInnerHTML={{__html: foo}}></div>
+
+var Section = React.createClass({
+  componentDidMount: function() {
+
+    Events.scrollEvent.register('begin', function() {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register('end', function() {
+      console.log("end", arguments);
+    });
+
+  },
+  scrollToTop: function() {
+    scroll.scrollToTop();
+  },
+  componentWillUnmount: function() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  },
+  render: function () {
+    return (
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Test 1</Link></li>
+                <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500}>Test 2</Link></li>
+                <li><Link activeClass="active" className="test3" to="test3" spy={true} smooth={true} duration={500} >Test 3</Link></li>
+                <li><Link activeClass="active" className="test4" to="test4" spy={true} smooth={true} duration={500}>Test 4</Link></li>
+                <li><Link activeClass="active" className="test5" to="test5" spy={true} smooth={true} duration={500} delay={1000}>Test 5 ( delay )</Link></li>
+                <li><DirectLink className="test6" to="anchor" spy={true} smooth={true} duration={500}>Test 6 (anchor)</DirectLink></li>
+                <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
+                <li> <a onClick={() => scroll.scrollMore(500)}>Scroll 500 More!</a></li>
+                <li> <a onClick={() => scroll.scrollMore(1000, { delay : 1500 })}>Scroll 1000 More! ( delay ) </a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <Element name="test1" className="element">
+          test1
+        </Element>
+
+        <Element name="test2" className="element">
+          test 2
+        </Element>
+
+        <Element name="test3" className="element">
+          test 3
+        </Element>
+
+        <Element name="test4" className="element">
+          test 4
+        </Element>
+
+        <Element name="test5" className="element">
+          test 5
+        </Element>
+
+        <div id="anchor" className="element">
+          test 6 (anchor)
+        </div>
+
+        <a onClick={this.scrollToTop}>To the top!</a>
+      
+      </div>
+    );
+  }
+});
 
 class Docs extends Component {
 
@@ -24,8 +100,7 @@ class Docs extends Component {
   render() {
     return (
       <div>
-        <div className="Docs" dangerouslySetInnerHTML={{__html: foo}}>
-        </div>
+        <Section />
       </div>
     );
   }
